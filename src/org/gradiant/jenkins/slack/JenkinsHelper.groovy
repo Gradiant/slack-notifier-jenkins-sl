83fa39b05003d48f9ec3268127e304cfae64ed8d
Fernando Vilas <vilasmaciel@gmail.com>
2019-04-12 09:12:36 +0200

    Avoid nulls on non multibranch jobs. Get the project name from the JOB_NAME environment variable

--- src/org/gradiant/jenkins/slack/JenkinsHelper.groovy
+++ src/org/gradiant/jenkins/slack/JenkinsHelper.groovy
@@ -2,54 +2,71 @@ package org.gradiant.jenkins.slack
 
 
 String getBranchName() {
-  return env.BRANCH_NAME
+    if (env.BRANCH_NAME != null) return env.BRANCH_NAME
+    return env.GIT_BRANCH
+}
+
+boolean isMultibranch() {
+    return getBranchName() != null
 }
 
 
 int getBuildNumber() {
-  return currentBuild.number
+    return currentBuild.number
 }
 
 
 String getAbsoluteUrl() {
-  return currentBuild.absoluteUrl
+    return currentBuild.absoluteUrl
 }
 
 
 String getProjectName() {
-  return env.BUILD_URL.split('job/')[1].split('/')[0]
+    if(isMultibranch()) return getMultiBranchProjectName()
+
+    return env.JOB_NAME
+}
+
+String getMultiBranchProjectName() {
+    String[] entries = env.JOB_NAME.split('/')
+    def len = entries.length
+
+    if (len == 1 || len == 2) return entries[0]
+
+    entries = entries.take(len - 1)
+    return entries.join('/')
 }
 
 
 List<String> getChanges() {
-  List<String> messages = []
-  for (int i = 0; i < currentBuild.changeSets.size(); i++) {
-    def entries = currentBuild.changeSets[i].items
-    for (int j = 0; j < entries.length; j++) {
-      def entry = entries[j]
-      messages.add("\t- ${entry.msg} [${entry.author}]")
+    List<String> messages = []
+    for (int i = 0; i < currentBuild.changeSets.size(); i++) {
+        def entries = currentBuild.changeSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            messages.add("\t- ${entry.msg} [${entry.author}]")
+        }
     }
-  }
 
-  return messages
+    return messages
 }
 
 String getDuration() {
-  return currentBuild.durationString.replace(' and counting', '')
+    return currentBuild.durationString.replace(' and counting', '')
 }
 
 
 String getCurrentStatus() {
-  return currentBuild.currentResult
+    return currentBuild.currentResult
 }
 
 
 String getPreviousStatus() {
-  def prev = currentBuild.previousBuild?.currentResult
+    def prev = currentBuild.previousBuild?.currentResult
 
-  if (!prev) {
-    return 'SUCCESS'
-  }
+    if (!prev) {
+        return 'SUCCESS'
+    }
 
-  return prev
+    return prev
 }
--- src/org/gradiant/jenkins/slack/SlackFormatter.groovy
+++ src/org/gradiant/jenkins/slack/SlackFormatter.groovy
@@ -2,16 +2,20 @@ package org.gradiant.jenkins.slack
 
 
 String format(String title = '', String message = '', String testSummary = '') {
-  def helper = new JenkinsHelper()
+    def helper = new JenkinsHelper()
 
-  def project = helper.getProjectName()
-  def branch = helper.getBranchName()
-  def buildNumber = helper.getBuildNumber()
-  def url = helper.getAbsoluteUrl()
+    def project = helper.getProjectName()
+    def branchName = helper.getBranchName()
+    def buildNumber = helper.getBuildNumber()
+    def url = helper.getAbsoluteUrl()
 
-  def result = "${project} » ${branch} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
-  if(message) result = result + "\nChanges:\n\t ${message.trim()}"
-  if(testSummary) result = result + "\n ${testSummary}"
+    def result = "${project}"
 
-  return result
+    if (branchName != null) result = "${result} » ${branchName}"
+
+    result = "${result} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
+    if (message) result = result + "\nChanges:\n\t ${message.trim()}"
+    if (testSummary) result = result + "\n ${testSummary}"
+
+    return result
 }
